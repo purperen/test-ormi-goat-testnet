9a611248312ee47c52d6a47b97537f1584fba5cb
purplezky <purplezky@gora.finance>
2024-11-14 07:32:45 +0100

    index RoleGranted events

--- src/minimal-oracle.ts
+++ src/minimal-oracle.ts
@@ -1,5 +1,6 @@
-import { UpdateQuoteCall, Cloned } from "../generated/MinimalOracleBTC/MinimalOracle"
-import { Quote, Clone } from "../generated/schema"
+import { UpdateQuoteCall, Cloned, RoleGranted } from "../generated/MinimalOracleBTC/MinimalOracle"
+import { Quote, Clone, GrantRole } from "../generated/schema"
+import { ByteArray } from "@graphprotocol/graph-ts";
 
 export function handleUpdateQuote(call: UpdateQuoteCall): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -15,6 +16,28 @@ export function handleUpdateQuote(call: UpdateQuoteCall): void {
   // Entity fields can be set based on call parameters
   entity.current = call.inputs.quote
   entity.updatedAt = call.block.timestamp
+  
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+}
+
+export function handleRoleGranted(event: RoleGranted): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = GrantRole.load(event.transaction.hash.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new GrantRole(event.transaction.hash.toHex())
+  }
+
+  // Entity fields can be set based on call parameters
+  entity.role = event.params.role
+  entity.grantee = event.params.account
+  entity.granter = event.params.sender
+  entity.grantedAt = event.block.timestamp
   
   // Entities can be written to the store with `.save()`
   entity.save()
