8ea7b535f641ce8052abfe22d620e93d84cc9af6
purplezky <purplezky@gora.finance>
2024-11-14 06:48:56 +0100

    added Cloned event and also index MinimalContractImplementation contract

--- src/minimal-oracle.ts
+++ src/minimal-oracle.ts
@@ -1,5 +1,5 @@
-import { UpdateQuoteCall } from "../generated/MinimalOracleBTC/MinimalOracle"
-import { Quote } from "../generated/schema"
+import { UpdateQuoteCall, Cloned } from "../generated/MinimalOracleBTC/MinimalOracle"
+import { Quote, Clone } from "../generated/schema"
 
 export function handleUpdateQuote(call: UpdateQuoteCall): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -15,6 +15,26 @@ export function handleUpdateQuote(call: UpdateQuoteCall): void {
   // Entity fields can be set based on call parameters
   entity.current = call.inputs.quote
   entity.updatedAt = call.block.timestamp
+  
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+}
+
+export function handleCloned(event: Cloned): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Clone.load(event.transaction.hash.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Clone(event.transaction.hash.toHex())
+  }
+
+  // Entity fields can be set based on call parameters
+  entity.newInstance = event.params.newInstance
+  entity.clonedAt = event.block.timestamp
   
   // Entities can be written to the store with `.save()`
   entity.save()
